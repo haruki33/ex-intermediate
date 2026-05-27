4932befd6e60c085984e4ed7a88e9bff16a14c1a
haruki33 <kkhk0869hs33@gmail.com>
2026-05-27 11:19:46 +0900

    [add] controller&html 条件に合うものがない場合の処理を実装

--- exam03/src/main/java/com/example/controller/ClothesController.java
+++ exam03/src/main/java/com/example/controller/ClothesController.java
@@ -33,19 +33,29 @@ public class ClothesController {
 
     /**
      * 渡された値で検索し、検索（結果）画面を表示する.
+     * <p>
+     * genderもしくはcolorがnull（未入力）の場合、エラーメッセージを表示。
+     * 検索した結果、条件に合うデータが存在しない場合、存在しない旨のメッセージを表示。
      *
      * @return 検索（結果）画面
      */
     @PostMapping("")
     public String search(String gender, String color, Model model) {
         model.addAttribute("isValid", false);
-
         if (gender == null || color == null) {
             model.addAttribute("isValid", true);
             model.addAttribute("message", "不正な値が指定されました。");
             return "search";
         }
+
+        model.addAttribute("isResultEmpty", false);
         List<Clothes> clothesList = service.showClothes(Integer.parseInt(gender), color);
+        if (clothesList.isEmpty()) {
+            model.addAttribute("isResultEmpty", true);
+            model.addAttribute("message", "検索条件にあうものはありません。");
+            return "search";
+        }
+
         model.addAttribute("clothesList", clothesList);
         return "search";
     }
